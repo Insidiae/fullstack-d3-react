83f4b1eb52faf3e77f25efa24ac97ce5a6fc442a
Kobe Ruado <insidiae423@gmail.com>
2022-05-17 01:42:26 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <p>Hello Vite + React + D3!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className={styles.appLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className={styles.appLink}
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
